00c8ee52b7bdff96f6ebded90b821a2925790956
dula9x <dulinanethmira626@gmail.com>
2025-07-20 08:51:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "VTFiSDRL#O9n3w7N4BPh5cijkDixKdAElgEIWeYmOAni4IWzjKxc",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:yeluZiuwEyTSNzBXtiotpYIqzPOLYwZQ@centerbeam.proxy.rlwy.net:55983",
   OWNER_NUM: process.env.OWNER_NUM || "94752978237",
 };
